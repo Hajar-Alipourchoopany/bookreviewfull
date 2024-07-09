5e7351d57962e6db7d1b8d4bb3120c05b7045195
unknown <onureredo@yahoo.com>
2024-07-09 13:50:33 +0200

    landingpage

--- Frontend/src/pages/LandingPage.jsx
+++ Frontend/src/pages/LandingPage.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // Import carousel styles
+import { Carousel } from 'react-responsive-carousel';
 
 const LandingPage = () => {
   const [topBooks, setTopBooks] = useState([]);
@@ -38,10 +40,57 @@ const LandingPage = () => {
     navigate('/register');
   };
 
+  const renderTopBooks = () => (
+    <Carousel
+      showArrows={true}
+      showThumbs={false}
+      showStatus={false}
+      infiniteLoop={true}
+      autoPlay={true}
+      interval={5000}
+      centerMode={true}
+      centerSlidePercentage={33.33}
+      emulateTouch
+    >
+      {topBooks.map((book) => (
+        <div key={book._id} className='p-4'>
+          <div className='relative pb-[150%]'>
+            <img
+              src={book.book_image}
+              alt={book.title}
+              className='absolute top-0 left-0 w-full h-full object-cover rounded-md'
+            />
+          </div>
+          <h3 className='text-lg font-semibold mb-1 text-center'>
+            {book.title}
+          </h3>
+          <div className='my-12'></div>
+        </div>
+      ))}
+    </Carousel>
+  );
+
+  const renderTopUsers = () => (
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+      {topUsers.map((user) => (
+        <div key={user._id} className='p-4 flex flex-col items-center'>
+          <img
+            src={user.profileImageUrl}
+            alt={user.username}
+            className='w-32 h-32 object-cover mb-4 rounded-full'
+          />
+          <h3 className='text-lg font-semibold mb-1 text-center'>
+            {user.username}
+          </h3>
+        </div>
+      ))}
+    </div>
+  );
+
   return (
-    <div className='min-h-screen flex flex-col'>
-      <div className='flex-1 flex flex-col items-center justify-center p-4'>
-        <div className='text-center'>
+    <div className='min-h-screen flex flex-col items-center bg-gray-100'>
+      <div className='w-full max-w-4xl p-4'>
+        <div className='text-center mb-12'>
           <h1 className='text-4xl font-bold mb-4'>
             Willkommen zu unserer Buch-Community
           </h1>
@@ -57,65 +106,15 @@ const LandingPage = () => {
             Registriere dich jetzt
           </Link>
         </div>
-        <div className='w-full max-w-4xl mt-8'>
-          <div className='mb-8'>
-            <h2 className='text-2xl font-semibold mb-4'>Top Bücher</h2>
-            <div className='flex overflow-x-auto space-x-4'>
-              {Array.isArray(topBooks) && topBooks.length > 0 ? (
-                topBooks.map((book) => (
-                  <div
-                    key={book._id}
-                    className='flex-shrink-0 w-48 p-2 bg-white rounded-lg shadow-md'
-                  >
-                    <img
-                      src={book.book_image}
-                      alt={book.title}
-                      className='w-full h-64 object-cover rounded-md'
-                    />
-                    <span className='block mt-2 text-center'>{book.title}</span>
-                    <button
-                      onClick={redirectToRegister}
-                      className='mt-2 w-full bg-blue-500 text-white py-1 rounded-md hover:bg-blue-700'
-                    >
-                      Watch Reviews
-                    </button>
-                  </div>
-                ))
-              ) : (
-                <p className='text-center'>Keine Top-Bücher gefunden</p>
-              )}
-            </div>
-          </div>
-          <div>
-            <h2 className='text-2xl font-semibold mb-4'>Top Users</h2>
-            <div className='flex overflow-x-auto space-x-4'>
-              {Array.isArray(topUsers) && topUsers.length > 0 ? (
-                topUsers.map((user) => (
-                  <div
-                    key={user._id}
-                    className='flex-shrink-0 w-48 p-2 bg-white rounded-lg shadow-md'
-                  >
-                    <img
-                      src={user.profileImageUrl}
-                      alt={user.username}
-                      className='w-full h-64 object-cover rounded-md'
-                    />
-                    <span className='block mt-2 text-center'>
-                      {user.username}
-                    </span>
-                    <button
-                      onClick={redirectToRegister}
-                      className='mt-2 w-full bg-blue-500 text-white py-1 rounded-md hover:bg-blue-700'
-                    >
-                      View Profile
-                    </button>
-                  </div>
-                ))
-              ) : (
-                <p className='text-center'>Keine Top-Benutzer gefunden</p>
-              )}
-            </div>
-          </div>
+
+        <div className='mb-12'>
+          <h2 className='text-2xl font-semibold mb-6'>Top Bücher</h2>
+          {renderTopBooks()}
+        </div>
+
+        <div>
+          <h2 className='text-2xl font-semibold mb-6'>Top Users</h2>
+          {renderTopUsers()}
         </div>
       </div>
     </div>
